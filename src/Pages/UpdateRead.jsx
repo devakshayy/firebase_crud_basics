97a1b5dceddb35b4ab90cbc4804c0fcb9df5c03c
devakshayy <devakshay916@gmail.com>
2025-01-28 16:14:45 +0530

    update read ie the id of each data logic completed

--- src/Pages/UpdateRead.jsx
+++ src/Pages/UpdateRead.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useState } from "react";
+import app from "../firebaseConfig";
+import { getDatabase,ref,get } from "firebase/database";
 
 const UpdateRead = () => {
+
+  let [initialData, setInitialData] = useState([]);
+   
+  const fetchData = async () => {
+    const db = getDatabase(app);
+    const dbRef = ref(db,"nature/fruits");
+    const snapShot = await get(dbRef);
+
+    if(snapShot.exists()){
+      const myData = snapShot.val();
+      const tempArray = Object.keys(myData).map(myFireId =>{
+         return {
+            ...myData[myFireId],
+            fruitId: myFireId
+         }
+      })
+      setInitialData(tempArray)
+    }else{
+      alert("An error occured")
+    }
+  }
+  
   return (
-    <div>
-      This is UpdateRead Page
+    <div className="flex flex-col items-center justify-center py-5 ">
+        <h1 className="text-2xl font-semibold uppercase ">This is UpdateRead page</h1>
+        
+        <button 
+          onClick={fetchData}
+          className='my-5 border border-gray-900 p-2 rounded-lg bg-violet-300 font-medium'
+          >Read Data
+       </button>
+       <ul>
+          {initialData.map((item,idx) => (
+            <li key={idx}>{item.fruitName} : {item.fruitDefinition} {item.fruitId}</li>
+          )) }
+       </ul>
+
     </div>
   )
 }
