8cd29f0bb8d8675c7bf0654561412f9883c5c4ec
devakshayy <devakshay916@gmail.com>
2025-01-29 20:50:24 +0530

    api key corrected

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain:import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
